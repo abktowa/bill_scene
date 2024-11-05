588fea5bd34174111db20bb34bd09a1067156e59
Shmuli Feld <shmulifeld@Shmulis-Air.quinnipiac.edu>
2024-11-05 14:52:24 -0500

    Added BasicShapes and Components classes

---
+++ BasicShapes.py
@@ -0,0 +1,12 @@
+"""
+This class contains the functions for drawing basic shapes
+with functions such as draw_rectangle(), draw_prism(), etc.
+"""
+
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
+
+class BasicShapes:
+    
+        
---
+++ Components.py
@@ -0,0 +1,11 @@
+"""
+This class contains the functions that draw the components in our scene
+with functions such as draw_pool_table(), draw_dice(), etc.
+"""
+
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
+
+class Components:
+	
